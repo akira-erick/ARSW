8c9ddea4f904fe7b5e456c1cfa80a7882059d30a
akira-erick <kajitani.motta@gmail.com>
2025-11-22 21:56:56 -0300

    feat(): criando serviço de ordens

--- order/app.py
+++ order/app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def make_order():
     return "Order service is running!"
 
+@app.route('/order/compensate', methods=['POST'])
+def make_order_compensation():
+    return "Order compensation executed!"
+
 if __name__ == '__main__':
     port = int(os.getenv('HOST_PORT_ORDER', 8081))
     app.run(debug=True, host='0.0.0.0', port = port)
