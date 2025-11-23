bff9fc4835b35b1848d5194f0172415a36513150
akira-erick <kajitani.motta@gmail.com>
2025-11-22 21:54:59 -0300

    feat(): criando serviço de ordens

---
+++ order/app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/order', methods=['POST'])
+def make_order():
+    return "Order service is running!"
+
+if __name__ == '__main__':
+    port = int(os.getenv('HOST_PORT_ORDER', 8081))
+    app.run(debug=True, host='0.0.0.0', port = port)
